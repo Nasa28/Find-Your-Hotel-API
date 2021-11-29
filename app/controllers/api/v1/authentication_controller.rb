6af804a318e3e1295b378ecdcf51a335e79f48c3
Nasa28 <kaluchinasaagu@gmail.com>
2021-11-29 23:16:26 +0100

    Add test for authentication controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,12 +1,17 @@
 module Api
   module V1
       class AuthenticationController < ApplicationController
-
+      rescue_from ActionController::ParameterMissing, with: :missing_parameter
         def create
+
+        
           render json: {token: '123'}, status: :created
         end
 
-        def destroy
+        private
+        def missing_parameter()
+          render json: {error: 'param is missing or the value is empty'}, status: :unprocessable_entity
+
         end
       end
   end
--- spec/request/authentication_spec.rb
+++ spec/request/authentication_spec.rb
@@ -3,11 +3,28 @@ require "rails_helper"
 describe 'Authentication', type: :request do
   describe 'POST authentication' do
     it "Authenticates the client" do
-      post '/api/v1/authenticate'
+      post '/api/v1/authenticate', params: {username: "chinasa", password: "password"}
       expect(response).to have_http_status(:created)
       expect(JSON.parse(response.body)).to eq({
        "token"=> "123"
       })
     end
+
+    it 'Should return error when username is not present' do
+      post '/api/v1/authenticate', params: {password: "password"}
+      expect(response).to have_http_status(:unprocessable_entity)
+       expect(JSON.parse(response.body)).to include({
+        'error' => 'param is missing or the value is empty'
+      })
+    end
+
+    it 'Should return error when username is not present' do
+      post '/api/v1/authenticate', params: {username: "chinasa"}
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(JSON.parse(response.body)).to eq({
+        'error' => 'param is missing or the value is empty'
+      })
+    end
+
   end
 end
