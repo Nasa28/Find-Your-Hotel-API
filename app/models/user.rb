c0a2c5df2e2d71eae978066de581733b3df0e210
Nasa28 <kaluchinasaagu@gmail.com>
2021-12-15 05:30:46 +0100

    Add validations to Favourite model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :favorites, dependent: :destroy
   has_many :favorite_hotels, through: :favorites, source: :hotel
+
+  validates :username, presence: true, length: { minimum: 4, maximum: 20 }, # format: { with: VALID_EMAIL_REGEX },
+                       uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
+
 end
