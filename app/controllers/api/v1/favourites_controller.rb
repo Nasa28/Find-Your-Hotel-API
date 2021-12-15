61ea839b8fc5ed24a0be9e6d8995584bf38098a1
Nasa28 <kaluchinasaagu@gmail.com>
2021-12-15 05:17:29 +0100

    Implement favourites create action

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -1,14 +1,32 @@
 module Api
   module V1
     class FavouritesController < ApplicationController
+       include ActionController::HttpAuthentication::Token
+
+      before_action :authenticate_user
       def index
+       
       end
 
       def create
+        hotel = Hotel.find(favorite_params[:hotel_id])
+       favorite = Favorite.new(user_id: @user.id, hotel_id: hotel.id)
+        if favorite.save
+          head :created
+        else
+          render json: { error: favorite.errors.messages }, status: :unprocessable_entity
+        end
       end
 
       def destroy
       end
+
+      private 
+
+        def favorite_params
+        params.require(:favorite).permit(:hotel_id)
+      end
+
     end
   end
 end
