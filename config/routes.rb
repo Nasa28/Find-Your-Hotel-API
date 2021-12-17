169f69e98cc8261923474d48f1e45cd5fda92e4e
Nasa28 <kaluchinasaagu@gmail.com>
2021-12-17 02:37:57 +0100

    Modify User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'favourites/index'
-  get 'favourites/create'
-  get 'favourites/destroy'
     namespace :api do
-    namespace :v1 do
+    namespace :v1 do 
+       post 'signup', to: 'user#create'
+       post 'login', to: 'authentication#create'
+       post 'logout', to: 'authentication#destroy'
      resources :hotels, only: [:index, :show]
      resources :favourites, only: [:index, :create, :destroy]
-     post 'authenticate', to: 'authentication#create'
    end
     end
 end
