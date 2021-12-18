9d7eacb3b95f1eb52c2ea3df097a31c44a6cf75f
Nasa28 <kaluchinasaagu@gmail.com>
2021-12-18 02:01:41 +0100

    Modify user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
 
   validates :username, presence: true, length: { minimum: 4, maximum: 20 }, # format: { with: VALID_EMAIL_REGEX },
                        uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 4 }, allow_nil: true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     namespace :api do
     namespace :v1 do 
        post 'signup', to: 'user#create'
-       post 'login', to: 'authentication#create'
+      post 'login', to: 'authentication#create'
        post 'logout', to: 'authentication#destroy'
      resources :hotels, only: [:index, :show]
      resources :favourites, only: [:index, :create, :destroy]
